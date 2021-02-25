757a2815ad4a928a28598cef831af22e8ca7048f
sunhuiquan <sunhuiquan@yeah.net>
2021-02-25 10:42:55 +0800

    commit

--- CH2/Programing/SMTP/SMTPclientPicuter.py
+++ CH2/Programing/SMTP/SMTPclientPicuter.py
@@ -11,13 +11,6 @@ passwordBase64 = 'd21sbWtyd3N3ZGNkZ2FjaQ=='
 # Choose a mail server (e.g. Google mail server) and call it mailserver
 mailserver = 'smtp.qq.com'  # Fill 1
 
-# Html and image file.
-with open("02.jpg", "rb") as f:
-    image_data = base64.b64encode(f.read())
-with open("hello.html", "rb") as f:
-    html_data = base64.b64encode(f.read())
-
-
 # Create socket called clientSocket and establish a TCP connection with mailserver
 # Fill 2
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -82,47 +75,48 @@ if recv7[:3] != '354':
 
 # Send message data.
 # Fill 6
-send_text='from:%s\r\nto:%s\nsubject:hello,you!\
-\r\nContent-Type:text/plain\r\n'%(emailFrom,emailTo)+'\r\n'+'hello'+'\r\n'
+send_text = 'from:%s\r\nto:%s\nsubject:hello,you!\
+\r\nContent-Type:text/plain\r\n' % (emailFrom, emailTo)+'\r\n'+'hello'+'\r\n'
 print(send_text)
-send_text=send_text.encode()
+send_text = send_text.encode()
 
-send_html='from:%s\nto:%s\nsubject:hello,you!\
-\nContent-Type:text/html\n'%(fromaddr,toaddr)+'\n'+'<h1>hello</h1><img src="https://github.com/sunhuiquan/A-Top-Down-Approach">'+'\r\n'
-send_html=send_html.encode()
+send_html = 'from:%s\nto:%s\nsubject:hello,you!\
+\r\nContent-Type:text/html\r\n' % (emailFrom, emailTo)+'\r\n'+'<h1>hello</h1><img src="https://github.com/sunhuiquan/mit_6.S081_lab/blob/util/IMG/lab1.png">'+'\r\n'
+print(send_html)
+send_html = send_html.encode()
 
-# f=open('gfriend.jpg','rb').read()
-# f=base64.b64encode(f) 
-# send_image=('from:%s\nto:%s\nsubject:hello,you!\
-# \nContent-Type:image/JPEG\nContent-transfer-encoding:base64\n'%(fromaddr,toaddr)+'\n').encode()+f+'\r\n'.encode() 
-# #需要指定图片的编码类型
+with open("02.jpg", "rb") as f:
+    f = base64.b64encode(f.read())
+send_image = ('from:%s\nto:%s\nsubject:hello,you!\
+\r\nContent-Type:image/JPEG\r\nContent-transfer-encoding:base64\r\n' % (emailFrom, emailTo)+'\r\n').encode()+f+'\r\n'.encode()
 
-# send_text_with_image='from:%s\nto:%s\nsubject:hello,you!\
-# \nContent-Type:multipart/mixed;boundary="simple"\n\n--simple\n'%(fromaddr,toaddr)+'Content-Type:text/html\n\n<h1>hello</h1><img src="https://pic3.zhimg.com/50/v2-29a01fdecc80b16e73160c40637a5e8c_hd.jpg">\n\n'
-# send_text_with_image=send_text_with_image.encode()+'--simple\n'.encode()+'Content-Type:image/JPEG\nContent-transfer-encoding:base64\n\n'.encode()
-# f=open('gfriend.jpg','rb').read()
-# f=base64.b64encode(f) 
-# send_text_with_image+=f
-# send_text_with_image+='\n--simple\r\n'.encode()
+send_text_with_image = 'from:%s\nto:%s\nsubject:hello,you!\
+\r\nContent-Type:multipart/mixed;boundary="simple"\r\n\r\n--simple\r\n' % (emailFrom, emailTo)+'Content-Type:text/html\r\n\r\n<h1>hello</h1><img src="https://github.com/sunhuiquan/mit_6.S081_lab/blob/util/IMG/lab1.png">\r\n\r\n'
+send_text_with_image = send_text_with_image.encode()+'--simple\r\n'.encode() + \
+    'Content-Type:image/JPEG\r\nContent-transfer-encoding:base64\r\n\r\n'.encode()
+with open("02.jpg", "rb") as f:
+    f = base64.b64encode(f.read())
+send_text_with_image += f
+send_text_with_image += '\r\n--simple\r\n'.encode()
 
-# temp=send_text_with_image
-# print(temp)
-# clientSocket.send(temp)
+temp = send_text_with_image
+print(temp)
+clientSocket.send(temp)
 
 
-# temp=endmsg
-# print(temp)
-# clientSocket.send(temp.encode())
-# recv = clientSocket.recv(1024).decode()
-# print(recv)
+temp = '\r\n.\r\n'
+print(temp)
+clientSocket.send(temp.encode())
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
-# # Send QUIT command and get server response.
-# # Fill 8
-# clientSocket.send('QUIT\r\n'.encode())
-# recv9 = clientSocket.recv(1024).decode()
-# print(recv9)
-# if recv9[:3] != '221':
-#     print('221 reply not received from server.')
+# Send QUIT command and get server response.
+# Fill 8
+clientSocket.send('QUIT\r\n'.encode())
+recv9 = clientSocket.recv(1024).decode()
+print(recv9)
+if recv9[:3] != '221':
+    print('221 reply not received from server.')
 
 # Close the client socket.
 clientSocket.close()
